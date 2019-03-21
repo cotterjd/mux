762b8a558948e0226d3ecb2f98ffccad318b4c5c
Jordan Cotter <jrdn.cttr@gmail.com>
2019-03-21 10:15:05 -0500

    initial commit

---
+++ sessions.sh
@@ -0,0 +1,11 @@
+cd ~/.tmuxinator
+tmuxinator home
+tmuxinator foo
+tmuxinator dynos_login
+tmuxinator mgmt_api
+tmuxinator mgmt_ui
+tmuxinator editor
+tmuxinator dynos_viewer
+tmuxinator mongo
+tmuxinator admin_ui
+tmuxinator dynos_viewer
